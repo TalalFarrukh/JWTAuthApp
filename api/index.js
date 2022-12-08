01dc3f495f9ceb94bfee4de90a138137f5203cc7
Talal <talalfarrukh200@gmail.com>
2022-12-08 22:23:49 +0500

    new line

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded())
 
+app.use(
+    express.static(path.join(__dirname, "../client/build"))
+)
+
 const users = [
     {
         id: "1",
